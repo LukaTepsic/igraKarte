8b7c77ce3be67f52437bbfe54c979c52840c1824
LukaTepsic <166146790+LukaTepsic@users.noreply.github.com>
2024-11-25 13:37:19 +0100

    Add files via upload

---
+++ igraNakarte/igraNakarte/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int karte = 0;
+        int cardCounter = 0;
+
+        while (cardCounter < 31)
+        {
+            Console.WriteLine("Unesite vrijednost karte (1 - 13): ");
+            karte = Convert.ToInt32(Console.ReadLine());
+
+            if (karte >= 1 && karte <= 13)
+            {
+                cardCounter += karte;
+            }
+            else
+            {
+                Console.WriteLine("Vrijednost mora biti između 1 i 13.");
+            }
+        }
+
+        if (cardCounter == 31)
+        {
+            Console.WriteLine("Pobjeda");
+        }
+        else
+        {
+            Console.WriteLine("Gubitak");
+        }
+        Console.ReadKey();
+    }
+}
